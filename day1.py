2503332f266916895ab5f6bdecc0d9622793eb53
Jigyasa-prep <dasjigyasa1437@gmail.com>
2026-01-15 16:13:00 +0530

    Day 1: Python setup and first program

---
+++ day1.py
@@ -0,0 +1,15 @@
+print("My placement prep has started.")
+#variables-act like containers to store value
+name = "Jigyasa"   # String type
+age = 20           # Number type
+#output
+print("Name:", name)
+print("Age:", age)
+#list-collection of items
+marks = [10, 20, 30, 40]  # 4 numbers
+print("Marks:", marks)
+#loop-doing same task multiple times
+for m in marks:
+    print(m)
+
+
