c527ac639791c89a7f1f9d6e5619f083bec71242
Jigyasa-prep <dasjigyasa1437@gmail.com>
2026-01-17 23:16:57 +0530

    Day 4: Shuffle the Array (lists and index)and two sum

--- day4.py
+++ day4.py
@@ -7,3 +7,12 @@ def shuffle(nums,n):
     return ans
 list=[1,2,3,4,5,6]
 print(shuffle(list,3))
+
+#two sum 
+def twosum(nums,target):
+    for i in range(len(nums)):
+        for j in range(i,len(nums)):
+            if nums[i]+nums[j]==target:
+                return[i,j]
+nums=[1,2,3,4]
+print(twosum(nums,5))
